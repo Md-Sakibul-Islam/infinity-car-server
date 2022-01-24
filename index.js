071655e210a9bdf47099c6a957c32932406a016c
Md Sakibul Islam <86571674+Md-Sakibul-Islam@users.noreply.github.com>
2022-01-24 23:54:58 +0600

    GET API with id

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-const cors = require('cors');
-require('dotenv').config()
-const app =express();
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
 
 const port = process.env.PORT || 5000;
-// middle ware 
-app.use(cors())
+// middle ware
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a2vjp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("infinity_car");
+    const carCollection = database.collection("car_collection");
 
-app.get('/',(req,res)=>{
-    res.send('hello car');
-})
+    // GET API
+    // getting all products here
+    app.get("/products", async (req, res) => {
+      const cursor = carCollection.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    });
 
-app.listen(port,()=>{
-    console.log('listing the port ',port)
-})
+    // GET API
+    // getting single products using id
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await carCollection.findOne(query);
+      res.json(result);
+    });
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("listing the port ", port);
+});
